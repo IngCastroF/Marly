b410ebfdfad3d2ca8d63ca580fa9352c7cf7493e
IngCastroF <73188985+IngCastroF@users.noreply.github.com>
2021-06-22 21:04:13 -0400

    Add files via upload

---
+++ Marly-main/formulario.js
@@ -0,0 +1,16 @@
+function Limpiar(){
+    document.getElementById("nombres").value = "";
+    document.getElementById("apellidos").value = "";
+    document.getElementById("correo").value = "";
+    document.getElementById("codigo_generado").value = "";
+    document.getElementById("curso").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("num_registro").value = "";
+    document.getElementById("titulo").value = "";
+    document.getElementById("fecha_despacho").value = "";
+    document.getElementById("codigo").value = "";
+    document.getElementById("autor").value = "";
+    document.getElementById("estante").value = "";
+    document.getElementById("fecha_entrega").value = "";
+    document.getElementById("mensaje").value = "";
+}
